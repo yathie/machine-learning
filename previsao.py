16fe48225bbd5944bfdff0cff9894cb88f5793f4
yathie <apenasjogos@yahoo.com.br>
2021-03-09 09:07:10 -0300

    substituindo cabeçalho

--- previsao.py
+++ previsao.py
@@ -12,6 +12,12 @@ df = pd.read_csv(url, header = None) #header = None -> indica que o arquivo não
 
 #df.head(n) mostra as primeiras n linhas do conjunto dados
 #df.tail(n) mostra as ultimas n linhas do conjunto dados
+#print(df.head(5))
+
+#atribuindo cabeçalho
+headers = ["symboling", "normalized-losses", "make", "fuel-type", "aspiration", "num-of-doors", "body-style", "drive-wheels", "engine-location", "wheel-base", "length", "width", "height", "curb-weight", "engine-type", "num-of-cylinders", "engine-size", "fuel-system", "bore", "stroke", "compression-ratio", "horsepower", "peak-rpm", "city-mpg", "highway-mpg", "price"]
+
+#substituindo o cabeçalho padrão pelo 'headers'
+df.columns = headers
+
 print(df.head(5))
-
-
