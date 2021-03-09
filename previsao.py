c372be092d186edc385f6dcc1887d58a3854abb4
yathie <apenasjogos@yahoo.com.br>
2021-03-09 10:53:14 -0300

    data types - statistical summary - concise summary

--- previsao.py
+++ previsao.py
@@ -26,3 +26,34 @@ df.columns = headers
 #automoveis.csv -> nome escolhido para salvar o arquivo
 path = "/home/.../cars.csv"
 df.to_csv(path)
+
+
+
+#metodos basicos do pandas
+
+#checar typos de dados de um conjunto de dados
+print(df.dtypes)
+
+#resumo estatístico de cada coluna
+#as métricas estatísticas podem dizer se há problemas matemáticos, como anomalias extremas e grandes desvios
+print(df.describe(include = "all"))
+
+'''
+count - total de linhas por coluna
+mean - valor médio da coluna
+std - desvio padrão
+min - valor mínimo
+25% - limite de quartis
+50%
+75%
+max - valor máximo
+
+.describe() -> ignora linhas e colunas sem números
+.describe(include = "all") -> mostra os resultados de todas as 26 colunas de df (colunas do tipo object). Apresentando um conjunto diferente de estatísticas:
+unique - número de objetos distintos na coluna
+top - objeto que ocorre com mais frequência
+freq - número de vezes que o objeto top aparece na coluna
+'''
+
+#.info() -> mostra 30 linhas superiores e inferiores do conjunto de dados. É um sumário conciso da base de dados
+print(df.info())
